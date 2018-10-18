7a1cd0e20cfd91daefab8d5ac0f74b951f128dbe
Adrian Jun Seraspi <adrianseraspi12@gmail.com>
2018-10-18 16:00:48 +0800

    Added SplashScreen

---
+++ Splash Screen/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
